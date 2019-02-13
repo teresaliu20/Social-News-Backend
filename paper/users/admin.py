126b4712360a4ec0840122276b465588dec09531
Teresa Liu <teresali@usc.edu>
2019-02-13 11:38:00 -0800

    Added followings and bookmark to admin dashboard

--- paper/users/admin.py
+++ paper/users/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
-
 from paper.users.forms import UserChangeForm, UserCreationForm
+from .models import Following, Bookmark
 
 User = get_user_model()
 
@@ -15,3 +15,14 @@ class UserAdmin(auth_admin.UserAdmin):
     fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
     list_display = ["username", "name", "is_superuser"]
     search_fields = ["name"]
+
+class FollowingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created', 'creator', 'following')
+
+
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created', 'creator', 'link')
+
+
+admin.site.register(Following, FollowingAdmin)
+admin.site.register(Bookmark, BookmarkAdmin)

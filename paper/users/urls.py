de75f9d42556268b0d1652053efd014db4e32511
William Wang <williamwang@Williams-MacBook-Pro.local>
2019-02-19 19:17:10 -0800

    added routes

--- paper/users/urls.py
+++ paper/users/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from paper.users.views import (
     user_list_view,
     user_redirect_view,
     user_update_view,
     user_detail_view,
+    bookmarks_list_view,
+    users_list_view,
+    user_bookmarks_view,
+    users_feed_view,
+    users_following_view,
+    users_social_feed_view
 )
 
 app_name = "users"
@@ -13,4 +21,12 @@ urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
+    path("all-bookmarks", view=bookmarks_list_view, name='bookmarks'),
+    path("show-all", view=users_list_view, name='usersall'),
+    path("<int:pk>/bookmarks", view=user_bookmarks_view, name='userbookmarks'),
+    path("<int:pk>/explore-feed", view=users_feed_view, name='userexplorefeed'),
+    path("<int:pk>/following", view=users_following_view, name='userfollowing'),
+    path("<int:pk>/social-feed", view=users_social_feed_view, name='usersocialfeed')
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- paper/users/views.py
+++ paper/users/views.py
@@ -1,7 +1,15 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
+from rest_framework.views import APIView
 from django.views.generic import DetailView, ListView, RedirectView, UpdateView
+from rest_framework.response import Response
+from .models import Bookmark, FeedSubscription, Following
+from .serializers import BookmarkSerializer, UserSerializer, FeedSubscriptionSerializer, FollowingSerializer
+from django.core import serializers
+import json
+from pprint import pprint
+from django.core.serializers.json import DjangoJSONEncoder
 
 User = get_user_model()
 
@@ -50,3 +58,131 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 
 
 user_redirect_view = UserRedirectView.as_view()
+
+# Returns all bookmarks in database
+class BookmarksListView(APIView):
+    def get(self, request, format=None):
+        bookmarks = Bookmark.objects.all()
+        serializer = BookmarkSerializer(bookmarks, many=True)
+        return Response(serializer.data)
+
+bookmarks_list_view = BookmarksListView.as_view()
+
+# Returns all users in database
+class UsersListView(APIView):
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+users_list_view = UsersListView.as_view()
+
+# Returns all bookmarks of a specific user
+class UserBookmarksView(APIView):
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk)
+        bookmarks = user.bookmark_set.all()
+        data = serializers.serialize('json', list(bookmarks), fields=('created', 'creator','link'))
+        return Response(json.loads(data))
+
+    def post(self, request, pk, format=None):
+        user = self.get_object(pk)
+        bLink = self.request.data.get('link')
+
+        if user and bLink:
+            b = Bookmark(link=bLink, creator=user)
+            b.save()
+            serializer = BookmarkSerializer(b)
+            return Response(serializer.data)
+        else:
+            return Response('Error: Unable to find either user or POST link data', status=HTTP_400_BAD_REQUEST)
+
+user_bookmarks_view = UserBookmarksView.as_view()
+
+# Returns all rss links that user is subscribed to
+class UserFeedView(APIView):
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk)
+        feed = user.feedsubscription_set.all()
+        data = serializers.serialize('json', list(feed), fields=('created', 'subscriber','rss_link'))
+        return Response(json.loads(data))
+
+    def post(self, request, pk, format=None):
+        user = self.get_object(pk)
+        fLink = self.request.data.get('link')
+
+        if user and fLink:
+            subscription = FeedSubscription(subscriber=user, rss_link=fLink)
+            subscription.save()
+            serializer = FeedSubscriptionSerializer(subscription)
+            return Response(serializer.data)
+        else:
+            return Response('Error: Unable to find either user or POST link data', status=HTTP_400_BAD_REQUEST)
+
+users_feed_view = UserFeedView.as_view()
+
+# Returns all the people a specific user is following
+class UserFollowingView(APIView):
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk)
+        following = user.friendship_creator_set.all()
+        data = serializers.serialize('json', list(following), fields=('created', 'creator','following'))
+        return Response(json.loads(data))
+
+    def post(self, request, pk, format=None):
+        user = self.get_object(pk)
+        following_id = self.request.data.get('user_id')
+        print(self.request.data.get('user_id'))
+
+        if user and following_id:
+            followingUser = User.objects.get(pk=following_id)
+            f = Following(creator=user, following=followingUser)
+            f.save()
+            serializer = FollowingSerializer(f)
+            return Response(serializer.data)
+        else:
+            return Response('Error: Unable to find either user or POST link data', status=HTTP_400_BAD_REQUEST)
+
+users_following_view = UserFollowingView.as_view()
+
+# Returns the social feed of a user: all bookmarks of people you are following
+class UserSocialFeedView(APIView):
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk)
+        following = user.friendship_creator_set.all()
+        data = json.loads(serializers.serialize('json', list(following), fields=('created', 'creator','following')))
+
+        socialFeed = []
+
+        for u in data:
+            followingUser = User.objects.get(pk=u["fields"]["following"])
+            bookmarks = followingUser.bookmark_set.all()
+            socialFeed += json.loads(serializers.serialize('json', list(bookmarks), fields=('created', 'creator','link')))
+
+        return Response(socialFeed)
+
+users_social_feed_view = UserSocialFeedView.as_view()
